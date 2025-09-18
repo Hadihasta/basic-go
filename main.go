2788597f91e386638257814726a0ef3bcb512847
hadiasta <hijrihadi@gmail.com>
2025-09-18 17:07:47 +0700

    init basic go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func print() {
+	fmt.Println("new learning")
+	fmt.Println(len("Learn"))
+}
+
+func main() {
+	var name string
+
+	name = "minez"
+
+	fmt.Println(name)
+
+
+	var kendaraan = "motor"
+	fmt.Println(kendaraan)
+}
+
