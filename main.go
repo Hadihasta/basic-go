0a0089ee61a8d45e244a29a8e4ff522f24052588
hadiasta <hijrihadi@gmail.com>
2025-09-18 19:16:57 +0700

    add const

--- main.go
+++ main.go
@@ -9,13 +9,33 @@ func print() {
 
 func main() {
 	var name string
-
 	name = "minez"
-
 	fmt.Println(name)
 
 
 	var kendaraan = "motor"
 	fmt.Println(kendaraan)
+
+	
+	// famouse declare 
+	a := "first declare"
+
+	a = "second declare"
+	fmt.Println(a)
+
+	// multiple declare
+	var (
+		firstName = "minez"
+		secondName = "zota"
+		thirdName = "enedi"
+	)
+
+		fmt.Println(firstName, secondName, thirdName)
+
+
+	// const if not use no error but cannot re-declare
+	const newName string = "hadi"
+	const lastName = "asta"
+
 }
 
